182a035ede24c0d7da971b0315ca0bc2bc1d1b9e
Francesco Bardozzo <lodeguns@dr.com>
2016-02-27 21:12:51 +0100

    Update MOOsEx.R

--- MOOsEx.R
+++ MOOsEx.R
@@ -216,5 +216,204 @@ osc.print.clusters(seq.osc.pa.pa.st7, treatlabtarget, "PAtoPAst27")
 
 
 
+###########################################################################################################
+##################################################################################################
+#   Figure 6 and Figure Online - Cophenetic Correlogram 
+#   Warning the computing requires several hours
+#
+################################################################################################
+
+library(classInt)
+library(KernSmooth)
+library(PST)
+library(cluster)
+
+#function
+clust.hc.diss <- function(seq.osc.caipa.pa.list, j){
+  seq.osc.l.in  <- seq.osc.caipa.pa.list[[j]][[1]]
+  seq.osc.l.out <- seq.osc.caipa.pa.list[[j]][[2]]
+  type.training <- paste(kegg.code, seq.osc.likh[[j]][[3]], sep = "  ")
+  
+  df.out   <- osc.get.diss.mat(seq.osc.l.out)
+  df.in    <- osc.get.diss.mat(seq.osc.l.in )
+  
+  diss.out <- daisy(df.out, metric = c("euclidean", "manhattan", "gower"),
+                    stand = FALSE, type = list())
+  diss.in  <- daisy(df.in, metric = c("euclidean", "manhattan", "gower"),
+                    stand = FALSE, type = list())
+  
+  hc1<-hclust(diss.out, method="complete")
+  hc2<-hclust(diss.in, method="complete")
+  return(list(hc1,hc2))
+}
+
+
+
+osc.caipa.caipa.corr <- list()
+
+for(j in 1:length(ktable.eco.sel[,1]))
+{ 
+  
+  path.index <- j
+  
+  osc.path.list       <-  osc.paths.list.caipa.operons[path.index]
+  osc.path.list[[1]]  <-  osc.path.list[[1]][osc.path.list[[1]][3] != "kmeans",]
+  osc.eco.mat         <-  osc.eco.operon.ms[[path.index]]
+  osc.w.out           <-  osc.list.w.out[path.index]
+  osc.w.in            <-  osc.list.w.in[path.index]
+  treat.path          <-  osc.paths.list.norfloxacin[path.index]
+  omic.val            <- "CAIPA"
+  h                   <- 2
+  n.seq               <- 3
+  
+  ptm <- proc.time()
+  
+  
+  ret <- tryCatch( val <- osc.pst.pred.diss(osc.path.list,
+                                            osc.eco.mat,
+                                            osc.w.in, 
+                                            osc.w.out,treat.path, 
+                                            omic.val, h, n.seq,
+                                            FALSE, TRUE) ,
+                   error = function(e) {
+                     print("Ops...")
+                     return(NULL)})
+  
+  
+  
+  if(is.null(ret))
+  {
+    osc.caipa.caipa.corr[length(osc.caipa.caipa.corr)+1] <- NULL
+  } else {
+    
+    osc.caipa.caipa.corr[length(osc.caipa.caipa.corr)+1] <- val }
+  }
+
+
+names(osc.caipa.caipa.corr)    <-  ktable.eco.sel$KEGGpath
+
+
+m.t <- osc.caipa.caipa.corr
+
+clean.t <- list()
+str<-c()
+for(j in 1:length(m.t))
+{
+  if(!is.null(m.t[j][[1]][[2]][[1]]))
+  { if(m.t[j][[1]][[2]][[1]]$l.s[1] != 0){
+    x<- length(clean.t)+1
+    clean.t[x] <- m.t[j]
+    clean.t[x][[1]][[3]] <- names(m.t[j])
+    str <- c(str, names(m.t[j]))
+    } else
+    {
+      print(j)
+      print(names(m.t[j]))
+    }
+  }
+  
+  
+}
+
+names(clean.t) <- str
+
+
+correlogram.caipa.caipa.d <- matrix(data = 0, 
+                                    nrow = length(clean.t), 
+                                    ncol = length(clean.t), byrow = FALSE,
+                                    dimnames = list(names(clean.t), 
+                                                    names(clean.t)))
+
+
+#n*n
+for(j in 1:length(clean.t)){
+  diss.j <- osc.get.diss.mat(clean.t[[j]][[2]])
+  
+  diss.j.cor <- diss.j
+  diss.j.cor[is.na(diss.j.cor)] <- 0
+  
+  diss.j.d <- daisy(diss.j, metric = c("euclidean", "manhattan", "gower"),
+                    stand = FALSE, type = list())
+  
+  ret <- tryCatch( hc1<-hclust(diss.j.d, method="complete") ,
+                   error = function(e) {
+                     print("1")
+                     return(NULL)})
+  
+  for(j1 in 1:length(clean.t))
+  { 
+    print(j1)
+    diss.j1 <- osc.get.diss.mat(clean.t[[j1]][[2]])
+    
+    diss.j1.cor <- diss.j1
+    diss.j1.cor[is.na(diss.j1.cor)] <- 0
+    
+    diss.j1.d <- daisy(diss.j1, metric = c("euclidean", "manhattan", "gower"),
+                       stand = FALSE, type = list())
+    ret1<-tryCatch( hc2<-hclust(diss.j1.d, method="complete") ,
+                    error = function(e) {
+                      print("2")
+                      return(NULL)})
+    if(is.null(ret) || is.null(ret1)){
+      correlogram.caipa.caipa.d[j,j1] <- NA
+    } else
+    {
+      correlogram.caipa.caipa.d[j,j1] <- round(cor(cophenetic(as.dendrogram(hc1)), cophenetic(as.dendrogram(hc2))), digits= 7)
+
+    }
+    
+  }
+  
+}
+
+
+
+ind <- apply(correlogram.caipa.caipa.d, 1, function(x) all(is.na(x)))
+
+
+
+correlogram.caipa.caipa.d <- correlogram.caipa.caipa.d[
+  colSums(is.na(correlogram.caipa.caipa.d)) != nrow(correlogram.caipa.caipa.d),
+  colSums(is.na(correlogram.caipa.caipa.d)) != nrow(correlogram.caipa.caipa.d)]
+
+
+
+library(d3heatmap)
+
+#Heatmap on the Paper
+d3heatmap(correlogram.caipa.caipa.d , 
+          scale = "none", 
+          colors = "Spectral",
+          dendrogram = "both", k_col = 9, k_row=9)
+
+save(correlogram.caipa.caipa.d,    file="correlogram.caipa.caipa.d.RData")
+
+
+
+correlogram.caipa.caipa.d.names <- correlogram.caipa.caipa.d
+colnames.d <- colnames(correlogram.caipa.caipa.d.names)
+
+for(j in 1:length(colnames.d))
+{
+  grep(colnames.d[j], ktable.eco.sel$KEGGpath)
+  colnames.d[j]<- paste(colnames.d[j],gsub( "-.-.*$", "", ktable.eco.sel[grep(colnames.d[j], ktable.eco.sel$KEGGpath),]$descr ), sep="-")
+  
+}
+colnames(correlogram.caipa.caipa.d.names)<-colnames.d
+rownames(correlogram.caipa.caipa.d.names)<-colnames.d
+
+#Heatmap online with complete descriptions
+d3heatmap(correlogram.caipa.caipa.d.names , 
+          scale = "none", 
+          colors = "Spectral",
+          dendrogram = "both", k_col = 9, k_row=9)
+
+
+save(correlogram.caipa.caipa.d.names,    file="correlogram.caipa.caipa.d.names.RData")
+
+
+########################################################################################
+
+
 
 
